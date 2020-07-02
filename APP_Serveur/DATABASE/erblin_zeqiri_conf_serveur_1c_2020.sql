7bb4e38ac8d71cc589ced385049ed2a34cacb9db
unknown <erblin.zeqiri17@gmail.com>
2020-07-02 21:08:15 +0200

    DSDSAF

--- APP_Serveur/DATABASE/erblin_zeqiri_conf_serveur_1c_2020.sql
+++ APP_Serveur/DATABASE/erblin_zeqiri_conf_serveur_1c_2020.sql
@@ -20,7 +20,7 @@ USE erblin_zeqiri_conf_serveur_1c_2020;
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 27 Mai 2020 à 12:29
+-- Généré le :  Jeu 02 Juillet 2020 à 18:56
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -45,7 +45,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `t_location` (
   `ID_Location` int(11) NOT NULL,
-  `Location` varchar(10) NOT NULL
+  `Location` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -53,8 +53,11 @@ CREATE TABLE `t_location` (
 --
 
 INSERT INTO `t_location` (`ID_Location`, `Location`) VALUES
-(1, 'Bureau1'),
-(2, 'Bureau2');
+(5, 'Bureau 1'),
+(6, 'Bureau 2'),
+(7, 'Bureau 3'),
+(8, 'Bureau 4'),
+(9, 'Bureau 5');
 
 -- --------------------------------------------------------
 
@@ -74,7 +77,11 @@ CREATE TABLE `t_mail` (
 INSERT INTO `t_mail` (`ID_Mail`, `Adresse_Mail`) VALUES
 (1, 'fedsad@dsds.cj'),
 (2, 'clowneriea'),
-(4, 'Jean@dujardin.ch');
+(4, 'Jean@dujardin.ch'),
+(5, 'pierre@bourne.com'),
+(9, 'nss@hotmail.com'),
+(11, 'tttttt@ttt.ch'),
+(13, 'asdas2@sadds.com');
 
 -- --------------------------------------------------------
 
@@ -94,14 +101,14 @@ CREATE TABLE `t_personne` (
 --
 
 INSERT INTO `t_personne` (`ID_Personne`, `Nom_Pers`, `Prenom_Pers`, `Date_Naissance_Pers`) VALUES
-(1, 'Zeqiri', 'Erblin', '1998-10-17'),
-(2, 'clowneriea', 'reererwre', '2018-11-22'),
+(1, 'Zeqiri', 'Erblin', '1998-09-17'),
 (4, 'Jean', 'Dujardin', '1985-11-25'),
-(6, 'Jean', 'Dujardin', '1985-11-25'),
-(8, 'asa', 'asdasd', '2020-03-10'),
-(10, 'adssad', 'rff', '2020-04-01'),
-(11, 'sdfsdfss', NULL, NULL),
-(13, 'bhhnhiik', NULL, NULL);
+(6, 'Jhon', 'Ofthegarden', '1985-11-25'),
+(8, 'Bite', 'Rivière', '2000-09-28'),
+(10, 'Dick', 'River', '2004-10-20'),
+(14, 'Bourn', 'Pierre', '1985-06-02'),
+(15, 'NSs', 'Zeqiri', '2007-06-07'),
+(16, 'ee', 'ee', '2004-11-30');
 
 -- --------------------------------------------------------
 
@@ -122,8 +129,10 @@ CREATE TABLE `t_pers_a_mail` (
 
 INSERT INTO `t_pers_a_mail` (`ID_Pers_A_Mail`, `FK_Personne`, `FK_Mail`, `Date_Mail`) VALUES
 (1, 1, 1, '2020-03-10 17:05:59'),
-(2, 2, 2, '2020-03-10 17:05:59'),
-(5, 4, 4, '2020-04-05 14:33:55');
+(5, 4, 4, '2020-04-05 14:33:55'),
+(6, 1, 4, '2020-06-22 23:12:14'),
+(7, 1, 5, '2020-06-23 09:54:43'),
+(8, 15, 9, '2020-06-23 13:46:24');
 
 -- --------------------------------------------------------
 
@@ -143,10 +152,15 @@ CREATE TABLE `t_pers_a_serveur` (
 --
 
 INSERT INTO `t_pers_a_serveur` (`ID_Pers_A_Serveur`, `FK_Personne`, `FK_Serveur`, `Date_Pers_Ask_Serveur`) VALUES
-(1, 2, 2, '2020-03-10 17:06:27'),
 (2, 1, 1, '2020-03-10 17:06:27'),
 (3, 1, 3, '2020-03-29 14:56:41'),
-(4, 1, 3, '2020-04-05 13:15:25');
+(4, 1, 3, '2020-04-05 13:15:25'),
+(5, 1, 8, '2020-06-23 09:55:42'),
+(7, 4, 8, '2020-06-23 09:55:42'),
+(8, 6, 8, '2020-06-23 09:55:42'),
+(9, 15, 7, '2020-06-23 13:49:31'),
+(10, 4, 2, '2020-07-02 16:32:30'),
+(11, 1, 10, '2020-07-02 16:36:32');
 
 -- --------------------------------------------------------
 
@@ -170,13 +184,15 @@ CREATE TABLE `t_serveur` (
 --
 
 INSERT INTO `t_serveur` (`ID_Serveur`, `Nom_Serv`, `Nombre_Port`, `Nombre_U`, `Date_Conf_Serv`, `Description`, `Puissance`, `Date_Serveur`) VALUES
-(1, 'cv vccvf', 2, 4, '2020-03-18', 'voila voila', 51, '2020-03-10 15:13:24'),
-(2, 'bvcxbcvbvbvc', 31121231, 2111, '2020-03-25', 'qweweqewq', 21321321, '2020-03-10 17:05:29'),
-(3, 'fdgdfdfg', 0, 0, '0000-00-00', 'voilà c\'est tellement rose', 0, '2020-03-24 19:42:46'),
+(1, 'rtzdfdrtzz', 2, 4, '2020-07-24', 'voila voila', 51, '2020-03-10 15:13:24'),
+(2, 'bvcxbcvbvbvc', 31121231, 2111, '2020-07-24', 'qweweqewq', 21321321, '2020-03-10 17:05:29'),
+(3, 'fdgdfdfg', 65, 45, '2020-07-15', 'voilà c\'est tellement rose', 0, '2020-03-24 19:42:46'),
 (5, 'serveur23', 234, 2111, '2020-05-15', 'werew', 323223, '2020-04-05 13:30:49'),
 (6, 'serveur23', 20, 12, '2020-05-15', 'wesh bien ou quoi', 2500, '2020-04-05 13:33:05'),
 (7, 'tg', 27, 27, '2020-05-20', 'tg bouffon\r\n', 5345, '2020-05-20 12:36:49'),
-(8, 'tg', 27, 27, '2020-05-20', 'tg bouffon', 5345, '2020-05-27 12:04:47');
+(8, 'tg', 27, 27, '2020-07-24', 'tg bouffon', 5345, '2020-05-27 12:04:47'),
+(9, 'rterr45dewe', 5, 5, '2020-07-16', 'reterege', 4, '2020-07-02 15:48:16'),
+(10, 'Serveur', 26, 26, '2020-07-30', 'Serveur', 2500, '2020-07-02 16:36:22');
 
 -- --------------------------------------------------------
 
@@ -196,9 +212,7 @@ CREATE TABLE `t_serv_a_location` (
 --
 
 INSERT INTO `t_serv_a_location` (`ID_Serv_A_Location`, `FK_Serveur`, `FK_Location`, `Date_Serv_A_Location`) VALUES
-(1, 2, 1, '2020-03-10 17:06:40'),
-(2, 1, 2, '2020-03-10 17:06:40'),
-(5, 6, 2, '2020-04-05 14:39:48');
+(7, 1, 5, '2020-07-02 16:40:33');
 
 -- --------------------------------------------------------
 
@@ -219,8 +233,9 @@ CREATE TABLE `t_serv_a_status` (
 
 INSERT INTO `t_serv_a_status` (`ID_Serv_A_Status`, `FK_Serveur`, `FK_Status`, `Date_Serv_A_Status`) VALUES
 (1, 2, 1, '2020-03-10 17:07:07'),
-(2, 2, 2, '2020-03-10 17:07:07'),
-(3, 6, 2, '2020-04-05 14:47:47');
+(4, 1, 1, '2020-06-23 11:50:58'),
+(6, 7, 1, '2020-07-02 16:14:03'),
+(7, 6, 7, '2020-07-02 16:28:44');
 
 -- --------------------------------------------------------
 
@@ -240,9 +255,10 @@ CREATE TABLE `t_serv_a_type_equipement` (
 --
 
 INSERT INTO `t_serv_a_type_equipement` (`ID_Serv_A_Type_Equipement`, `Fk_Serveur`, `FK_Type_Equipement`, `Date_Serv_A_Type_Equipement`) VALUES
-(1, 1, 2, '2020-03-10 17:07:40'),
-(2, 2, 2, '2020-03-10 17:07:40'),
-(3, 6, 3, '2020-04-05 14:52:33');
+(3, 6, 3, '2020-04-05 14:52:33'),
+(4, 1, 1, '2020-07-02 16:30:43'),
+(5, 2, 3, '2020-07-02 16:30:50'),
+(6, 2, 6, '2020-07-02 16:30:50');
 
 -- --------------------------------------------------------
 
@@ -261,9 +277,10 @@ CREATE TABLE `t_status` (
 
 INSERT INTO `t_status` (`ID_Status`, `Status`) VALUES
 (1, 'Traité'),
-(2, 'clowneriea'),
-(3, 'voilà c\'es'),
-(5, 'tkt');
+(7, 'Désangager'),
+(8, 'Traité'),
+(9, 'A traité'),
+(10, 'asdew3');
 
 -- --------------------------------------------------------
 
@@ -282,9 +299,9 @@ CREATE TABLE `t_type_equipement` (
 
 INSERT INTO `t_type_equipement` (`ID_Type_Equipement`, `Type_Equipement`) VALUES
 (1, 'cxvxvxcv'),
-(2, 'clowneriea'),
 (3, 'voilà c\'est tellement rose'),
-(5, 'adsad');
+(6, 'fdgfdgdfgdfgfdgdfdf'),
+(7, 'asdw2asdas22');
 
 --
 -- Index pour les tables exportées
@@ -374,57 +391,57 @@ ALTER TABLE `t_type_equipement`
 -- AUTO_INCREMENT pour la table `t_location`
 --
 ALTER TABLE `t_location`
-  MODIFY `ID_Location` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_Location` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT pour la table `t_mail`
 --
 ALTER TABLE `t_mail`
-  MODIFY `ID_Mail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_Mail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT pour la table `t_personne`
 --
 ALTER TABLE `t_personne`
-  MODIFY `ID_Personne` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ID_Personne` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT pour la table `t_pers_a_mail`
 --
 ALTER TABLE `t_pers_a_mail`
-  MODIFY `ID_Pers_A_Mail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID_Pers_A_Mail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `t_pers_a_serveur`
 --
 ALTER TABLE `t_pers_a_serveur`
-  MODIFY `ID_Pers_A_Serveur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_Pers_A_Serveur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT pour la table `t_serveur`
 --
 ALTER TABLE `t_serveur`
-  MODIFY `ID_Serveur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID_Serveur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `t_serv_a_location`
 --
 ALTER TABLE `t_serv_a_location`
-  MODIFY `ID_Serv_A_Location` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID_Serv_A_Location` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `t_serv_a_status`
 --
 ALTER TABLE `t_serv_a_status`
-  MODIFY `ID_Serv_A_Status` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_Serv_A_Status` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `t_serv_a_type_equipement`
 --
 ALTER TABLE `t_serv_a_type_equipement`
-  MODIFY `ID_Serv_A_Type_Equipement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_Serv_A_Type_Equipement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `t_status`
 --
 ALTER TABLE `t_status`
-  MODIFY `ID_Status` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID_Status` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `t_type_equipement`
 --
 ALTER TABLE `t_type_equipement`
-  MODIFY `ID_Type_Equipement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID_Type_Equipement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- Contraintes pour les tables exportées
 --
